cf5f99a1097e952d09a28ee8d16b135825f5ed51
Yatin Kundra <100512750+yatin-kundra@users.noreply.github.com>
2024-03-04 11:36:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,18 @@ import requests
 
 # Function to empty a folder
 def empty_folder(folder_path):
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    if os.path.exists(folder_path):
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    os.rmdir(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
+    else:
+        print(f"The folder {folder_path} does not exist.")
 
 # Function to download videos and extract audio
 def download_and_extract_audio(artist_names, num_songs):
